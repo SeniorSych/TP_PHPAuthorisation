e113390b7d703b90cceca1a2e4e7ce03f4dd3342
SeniorSych <tatarlar1990@gmail.com>
2023-10-05 13:22:54 +0300

    fixed missed link

--- index.php
+++ index.php
@@ -16,24 +16,26 @@ if ($_SESSION['user'] ?? '')
 </head>
 <body>
 <!--Форма авторизации-->
-<form action="vendor/singin.php" method="post">
+<form action="vendor/signin.php" method="post">
     <label>Логин</label>
-    <input type="text" name="username" required placeholder="Введите логин">
+    <input type="text" name="username"  placeholder="Введите логин">
     <label>Пароль</label>
-    <input type="password" name="pass" required placeholder="Введите пароль">
-    <button>Войти</button>
+    <input type="password" name="pass"  placeholder="Введите пароль">
+    <button type="submit" class="login-btn">Войти</button>
     <p>
         Нет аккаунта? - <a href="register.php">Регистрация</a>!
     </p>
     <?php
-    if ($_SESSION['message'] ?? '')
-    {
-        echo '<p class="msg">'.$_SESSION['message'].'</p>';
+    if ($_SESSION['message'] ?? '') {
+        echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
     }
     unset($_SESSION['message']);
     ?>
 </form>
+<script src="assets/js/jquery-3.7.0.slim.js">
+<script src="assets/js/main.js">
 
+</script>
 
 </body>
 </html>

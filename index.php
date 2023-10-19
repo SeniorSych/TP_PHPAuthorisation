78c0a2ac26f9dd57bd6cef289737e90505a10ccc
SeniorSych <tatarlar1990@gmail.com>
2023-10-19 13:13:50 +0300

    small front-end changes in login page

--- index.php
+++ index.php
@@ -16,22 +16,29 @@ if ($_SESSION['user'] ?? '')
 </head>
 <body>
 <!--Форма авторизации-->
-<form action="vendor/signin.php" method="post">
-    <label>Логин</label>
-    <input type="text" name="username"  placeholder="Введите логин">
-    <label>Пароль</label>
-    <input type="password" name="pass"  placeholder="Введите пароль">
-    <button type="submit" class="login-btn">Войти</button>
-    <p>
-        Нет аккаунта? - <a href="register.php">Регистрация</a>!
-    </p>
-    <?php
-    if ($_SESSION['message'] ?? '') {
-        echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
-    }
-    unset($_SESSION['message']);
-    ?>
-</form>
+<div class="myform">
+    <h4>Вход в профиль</h4>
+    <div class="login">
+        <form action="vendor/signin.php" method="post">
+
+            <input type="text" name="username"  placeholder="Введите логин">
+
+            <input type="password" name="pass"  placeholder="Введите пароль">
+            <button type="submit" class="login-btn">Войти</button>
+            <div class="reg">
+                <p>
+                    Нет аккаунта? - <a href="register.php">Регистрация</a>!
+                </p>
+            </div>
+            <?php
+            if ($_SESSION['message'] ?? '') {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+            ?>
+        </form>
+    </div>
+</div>
 <script src="assets/js/jquery-3.7.0.slim.js">
 <script src="assets/js/main.js">
 
